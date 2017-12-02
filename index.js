c5fbfb687eabb446963271f24fc7b076ad5d1dec
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2017-12-02 18:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if string.toLowerCase() === string {
+  if (string).toLowerCase() === (string) {
     return "I can't hear you!"
   }
-  else if string === "I love you, Grandma" {
+  else if (string) === "I love you, Grandma" {
     return "I love you, too."
   }
   else {
